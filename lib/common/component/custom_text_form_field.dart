4586057ded7a1dfb88693aca267edc3e30d7c29c
kim <k2h2j3@gmail.com>
2024-02-28 15:47:21 +0900

    text폼 정의 및 아이디 비밀번호 입력창 구현

---
+++ lib/common/component/custom_text_form_field.dart
@@ -0,0 +1,59 @@
+import 'package:actual/common/const/colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final String? hintText;
+  final String? errorText;
+  final bool obscureText;
+  final bool autofocus;
+  final ValueChanged<String>? onChanged;
+
+  const CustomTextFormField({
+    required this.onChanged,
+    this.hintText,
+    this.errorText,
+    this.obscureText = false,
+    this.autofocus = false,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final baseBorder = OutlineInputBorder(
+      borderSide: BorderSide(
+        color: INPUT_BORDER_COLOR,
+        width: 1.0,
+      )
+    );
+
+    return TextFormField(
+      cursorColor: PRIMARY_COLOR,
+      // 비밀번호 입력할 때,
+      obscureText: obscureText,
+      // 입력창에 바로 포커싱 되도록
+      autofocus: autofocus,
+      // 값이 바뀔때마다 실행되는 콜백
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        contentPadding: EdgeInsets.all(20),
+        hintText: hintText,
+        errorText: errorText,
+        hintStyle: TextStyle(
+          color: BODY_TEXT_COLOR,
+          fontSize: 14.0,
+        ),
+        fillColor: INPUT_BG_COLOR,
+        // false - 배경색 없음
+          // true - 배경색 있음
+        filled: true,
+        // 모든 Input 상태의 기본 스타일 세팅
+        border: baseBorder,
+        focusedBorder: baseBorder.copyWith(
+          borderSide: baseBorder.borderSide.copyWith(
+            color: PRIMARY_COLOR,
+          ),
+        ),
+      ),
+    );
+  }
+}
