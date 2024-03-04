c07ddc5e5e58e9ac2876effabcc6ef6db9024a35
kim <k2h2j3@gmail.com>
2024-03-04 12:04:27 +0900

    Product_card 구현

--- lib/restaurant/view/restaurant_detail_screen.dart
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:actual/common/layout/default_layout.dart';
+import 'package:actual/product/component/product_card.dart';
 import 'package:actual/restaurant/component/restaurant_card.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +11,7 @@ class RestaurantDetailScreen extends StatelessWidget {
     return DefaultLayout(
       title: '불타는 떡볶이',
       child:
+
       Column(
         children: [
         RestaurantCard(
@@ -24,6 +26,10 @@ class RestaurantDetailScreen extends StatelessWidget {
           ratings: 4.76,
           isDetail: true,
           detail: '맛있는 떡볶이',
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: ProductCard(),
         ),
       ],
     ),);
