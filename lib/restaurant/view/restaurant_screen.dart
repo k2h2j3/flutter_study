6c671a68bd9fa97ab600a1118c40d10476523022
kim <k2h2j3@gmail.com>
2024-03-04 10:17:48 +0900

    JSON 데이터 매핑하기

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -1,5 +1,6 @@
 import 'package:actual/common/const/data.dart';
 import 'package:actual/restaurant/component/restaurant_card.dart';
+import 'package:actual/restaurant/model/restaurant_model.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
@@ -12,10 +13,10 @@ class RestaurantScreen extends StatelessWidget {
     final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
 
     final resp = await dio.get('http://$ip/restaurant',
-        options: Options(
-        headers: {
-        'authorization': 'Bearer $accessToken',
-        }
+      options: Options(
+          headers: {
+            'authorization': 'Bearer $accessToken',
+          }
       ),
     );
 
@@ -28,42 +29,56 @@ class RestaurantScreen extends StatelessWidget {
     return Container(
       child: Center(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: FutureBuilder<List>(
-            future: paginateRestaurant(),
-            builder: (context, AsyncSnapshot<List> snapshot){
-              if(!snapshot.hasData){
-                return Container();
-              }
-              
-              return ListView.separated(
-                itemCount: snapshot.data!.length,
-                itemBuilder: (_, index){
-                  final item = snapshot.data![index];
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: FutureBuilder<List>(
+              future: paginateRestaurant(),
+              builder: (context, AsyncSnapshot<List> snapshot) {
+                if (!snapshot.hasData) {
+                  return Container();
+                }
 
-                  return RestaurantCard(
-                    image: Image.network(
-                      'http://$ip${item['thumbUrl']}',
-                      fit: BoxFit.cover,
-                    ),
-                    // image: Image.asset(
-                    //   'asset/img/food/ddeok_bok_gi.jpg',
-                    //   fit: BoxFit.cover,
-                    // ),
-                    name: item['name'],
-                    tags: List<String>.from(item['tags']),
-                    ratingsCount: item['ratingsCount'],
-                    deliveryTime: item['deliveryTime'],
-                    deliveryFee: item['deliveryFee'],
-                    ratings: item['ratings'],
-                  );
-                },
-                separatorBuilder: (_, index){
-                  return SizedBox(height: 16.0);
-                },
-              );
-            },
-          )
+                return ListView.separated(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (_, index) {
+                    final item = snapshot.data![index];
+                    // parsed
+                    final pItem = RestaurantModel(
+                        id: item['id'],
+                        name: item['name'],
+                        thumbUrl: 'http://$ip${item['thumbUrl']}',
+                        tags: List<String>.from(item['tags']),
+                        priceRange: RestaurantPriceRange.values.firstWhere(
+                              (e) =>e.name == item['priceRange'],
+                        ),
+                        ratings: item['ratings'],
+                        ratingsCount : item['ratingsCount'],
+                        deliveryTime: item['deliveryTime'],
+                        deliveryFee: item['deliveryFee'],
+                    );
+
+                    return RestaurantCard(
+                      image: Image.network(
+                        pItem.thumbUrl,
+                        fit: BoxFit.cover,
+                      ),
+                      // image: Image.asset(
+                      //   'asset/img/food/ddeok_bok_gi.jpg',
+                      //   fit: BoxFit.cover,
+                      // ),
+                      name: pItem.name,
+                      tags: pItem.tags,
+                      ratingsCount: pItem.ratingsCount,
+                      deliveryTime: pItem.deliveryTime,
+                      deliveryFee: pItem.deliveryFee,
+                      ratings: pItem.ratings,
+                    );
+                  },
+                  separatorBuilder: (_, index) {
+                    return SizedBox(height: 16.0);
+                  },
+                );
+              },
+            )
         ),
       ),
     );
