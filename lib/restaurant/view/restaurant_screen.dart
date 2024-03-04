ccca729fd5c86cdc67a484d0a96f02e127cbb3c8
kim <k2h2j3@gmail.com>
2024-03-04 14:45:09 +0900

    Product_detail 상세내용 구현

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -33,7 +33,9 @@ class RestaurantScreen extends StatelessWidget {
               future: paginateRestaurant(),
               builder: (context, AsyncSnapshot<List> snapshot) {
                 if (!snapshot.hasData) {
-                  return Container();
+                  return Center(
+                    child: CircularProgressIndicator(),
+                  );
                 }
 
                 return ListView.separated(
@@ -48,7 +50,9 @@ class RestaurantScreen extends StatelessWidget {
                       onTap: () {
                         Navigator.of(context).push(
                           MaterialPageRoute(
-                            builder: (_) => RestaurantDetailScreen(),
+                            builder: (_) => RestaurantDetailScreen(
+                              id: pItem.id,
+                            ),
                           ),
                         );
                       },
