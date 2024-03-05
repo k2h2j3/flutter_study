22117af9608459b790c1afa46cbf7387b913cb14
kim <k2h2j3@gmail.com>
2024-03-05 09:34:58 +0900

    restaurant_detail JsonSerializable 적용

--- lib/restaurant/model/restaurant_detail_model.dart
+++ lib/restaurant/model/restaurant_detail_model.dart
@@ -1,6 +1,11 @@
 import 'package:actual/common/const/data.dart';
+import 'package:actual/common/utils/data_utils.dart';
 import 'package:actual/restaurant/model/restaurant_model.dart';
+import 'package:json_annotation/json_annotation.dart';
 
+part 'restaurant_detail_model.g.dart';
+
+@JsonSerializable()
 class RestaurantDetailModel extends RestaurantModel {
   final String detail;
   final List<RestaurantProdcutModel> products;
@@ -19,35 +24,17 @@ class RestaurantDetailModel extends RestaurantModel {
     required this.products,
   });
 
-  factory RestaurantDetailModel.fromJson({
-    required Map<String, dynamic> json,
-  }) {
-    return RestaurantDetailModel(
-      id: json['id'],
-      name: json['name'],
-      thumbUrl: 'http://$ip${json['thumbUrl']}',
-      tags: List<String>.from(json['tags']),
-      priceRange: RestaurantPriceRange.values
-          .firstWhere((e) => e.name == json['priceRange']),
-      ratings: json['ratings'],
-      ratingsCount: json['ratingsCount'],
-      deliveryTime: json['deliveryTime'],
-      deliveryFee: json['deliveryFee'],
-      detail: json['detail'],
-      products: json['products']
-          .map<RestaurantProdcutModel>(
-            (x) => RestaurantProdcutModel.fromJson(
-              json: x,
-            ),
-          )
-          .toList(),
-    );
-  }
+  factory RestaurantDetailModel.fromJson(Map<String, dynamic>json)
+  => _$RestaurantDetailModelFromJson(json);
 }
 
+@JsonSerializable()
 class RestaurantProdcutModel {
   final String id;
   final String name;
+  @JsonKey(
+    fromJson: DataUtils.pathToUrl,
+  )
   final String imgUrl;
   final String detail;
   final int price;
@@ -60,15 +47,6 @@ class RestaurantProdcutModel {
     required this.price,
   });
 
-  factory RestaurantProdcutModel.fromJson({
-    required Map<String, dynamic> json,
-  }) {
-    return RestaurantProdcutModel(
-      id: json['id'],
-      name: json['name'],
-      imgUrl: 'http://$ip/${json['imgUrl']}',
-      detail: json['detail'],
-      price: json['price'],
-    );
-  }
+  factory RestaurantProdcutModel.fromJson(Map<String, dynamic> json)
+  => _$RestaurantProdcutModelFromJson(json);
 }
